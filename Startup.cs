8018c3619605fece2bf597302f75d4d3c1eae7ed
HerRoyalWeirdo <1826083@ucolcloud.ac.nz>
2021-08-28 02:53:05 +1200

    update?

---
+++ Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseExceptionHandler("/error.html");
+            //  if (configuration["EnableDeveloperExceptions"] == "True")
+            //env.IsDevelopment()
+            if (configuration.GetValue<bool>("EnableDeveloperExceptions"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+
+                await next();
+            });
+            //middleware
+            app.UseFileServer();
+            //basics - 4 for erroe custom page
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello Nothing!");//chganging then savimng then reloading without re runnign avctually works
+            //        //ctrl + f5 for no debugging
+            //    });
+            //});
+        }
+    }
+}
