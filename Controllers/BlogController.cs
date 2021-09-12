e4121a8c40c4bf73a3fb911414f872038b5679bc
HerRoyalWeirdo <1826083@ucolcloud.ac.nz>
2021-09-12 18:40:53 +1200

    testing url change

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -15,12 +15,16 @@ namespace ExploreCalifornia.Controllers
             //View();
         }
 
-        public IActionResult Post(int id = -1)
+        [Route("blog/{year:int}/{month:int}/{key}")]
+        public IActionResult Post(int year, int month, string key)//int id = -1)
         {//the url will show the 'id' on the page
            // if(id == null)
              //   return new ContentResult { Content = "null" };
            // else
-            return new ContentResult {Content = id.ToString()};
+            return new ContentResult {
+                Content = string.Format("Year: {0}; Month: {1}; Key: {2}", year, month, key)
+                          //id.ToString()
+            };
             //model binding
         }
     }
